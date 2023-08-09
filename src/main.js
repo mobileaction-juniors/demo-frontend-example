81164635e573ac779309cc5c785c43f63ade0f5e
Tuna Bozkurt <tuna.bozkurt@ug.bilkent.edu.tr>
2023-08-09 17:57:39 +0300

    feat(ONB-201) Completed the main KeywordGenerator.vue functionality.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import TheHeader from './components/TheHeader.vue';
 import TheNav from './components/TheNav.vue';
 import FontAwesomeIcon from './fontAwesome';
 
+
+
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('the-header', TheHeader);
 Vue.component('the-nav', TheNav);
@@ -18,3 +20,5 @@ new Vue({
     render: h => h(App),
     router,
 }).$mount('#app');
+
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,23 @@
+
+
+
+
 <template>
     <div>
-        <div style="text-align: center">
-            <h1>Welcome to Onboarding Progress </h1>
-            <p>You can see the your first task details via link. Please continue that steps while coding. </p>
-            <a href="https://mobileaction.atlassian.net/wiki/spaces/ENG/pages/1197015045/Implement+Your+First+Task+-+FrontEnd" target="_blank"> Implement First Front-End Task </a>
-        </div>
+      <div class="ma-elements-holder">
+        <MaKeywordGenerator />
+      </div>
     </div>
 </template>
 
+
+
 <script>
-    export default {
-        name: 'index',
-    };
+  import MaKeywordGenerator from '@/pages/keyword-generator/KeywordGenerator.vue';
+  export default {
+    name: 'index',
+    components: { MaKeywordGenerator }, //this registers the component
+  };
 </script>
 
 <style scoped>
