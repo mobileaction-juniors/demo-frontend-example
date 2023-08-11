e63b36ea7b35014ad2f7df12a16b4c6534d6becc
Tuna Bozkurt <tuna.bozkurt@ug.bilkent.edu.tr>
2023-08-11 13:55:06 +0300

    feat(ONB-202-4) Changed UI design & Removed duplicate phrases.

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -2,33 +2,36 @@
 
 <template>
   <div class="ma-keywords-generator">
-    <div class="ma-header">
-      <span>Keyword Generator: </span>
+<!--    <div class="ma-header">-->
+      <span class="ma-title">Keyword Generator </span>
 
       <input v-model="inputText" placeholder="Enter text">
-      <button @click="storeText">Enter</button>
-
+      <Button type="primary"   @click="storeText">Enter</Button>
 
       <select v-model="nGramLength">
         <option :value="num" v-for="num in dropdownAr" :key="num.id">{{num}}</option>
       </select>
+      <div class="kutucukDiv">
 
-      <p>
-        <span v-for="(word, index) in computedWords" :key="index"> {{ word }}
-          {{ index !== computedWords.length - 1 ? ', ' : '' }}
-        </span>
-      </p>
+          <span v-for="(word, index) in computedWords" :key="index"> {{ word }}
+            {{ index !== computedWords.length - 1 ? ', ' : '' }}
+          </span>
 
+      </div>
 
     </div>
-  </div>
+<!--  </div>-->
 </template>
 <script>
 
 import {regex, filterArr} from "@/cleanupResources";
+import {Button} from 'ant-design-vue';
+// import 'ant-design-vue/dist/antd.css';
 
 export default {
-
+  components:{
+    Button,
+  },
   data() {
     return {
       inputText: '',
@@ -38,11 +41,9 @@ export default {
       dropdownAr:[],
     };
   },//vue dev tools
-
   created(){
     this.dropdownAr = this.createAr(10);
   },
-
   methods: {
     getCleanInput(text){
       text = text.replace(regex,''); //remove special characters
@@ -70,20 +71,51 @@ export default {
       const ans = [];
       for(let i=0;i<splitWords.length-n+1;i++){
         const temp = [];
-        for(let k=i;k<i+n;k++) temp.push(splitWords[k]);
-        const s = temp.join(' ');
-        ans.push(s);
+        for(let k=i;k<splitWords.length;k++){
+          let flag=0;
+          for(let j=0;j<temp.length;j++){
+            if(splitWords[k] === temp[j]){
+              flag=1;
+              break;
+            }
+          }
+          if(flag===0)temp.push(splitWords[k]);
+          if(temp.length===n)break;
+        }
+        if(temp.length===n){const s = temp.join(' ');ans.push(s);}
       }
-
-      return ans;
+      return Array.from(new Set(ans));
     },
     storeText() {
       this.computedWords = this.computeWords(this.getCleanInput(this.inputText),this.nGramLength);
     }
   }
 };
-
 </script>
 
 
+<style scoped>
+.ma-keywords-generator *{
+  margin: 5px;
+}
+.ma-keywords-generator{
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+
+.ma-title{
+  text-align: center;
+  color: cornflowerblue;
+}
+
+.kutucukDiv{
+  min-height: 100px;
+  border: 1px solid black;
+  text-align: center;
+
+}
+
+</style>
 
