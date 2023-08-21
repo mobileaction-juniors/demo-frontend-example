8d7af352357a2c378fa7ea40c462072e8428a9db
kerimeren <kerimeren17@gmail.com>
2023-08-21 15:42:03 +0300

    feat(onb-203): Do the suggested changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
