1f81a42ec8925f4a87d5153564e966f499a23d26
Doğa Elif Konuk <dogaelifkonuk@gmail.com>
2024-07-04 10:24:51 +0300

    fix(ONB-201): keyword generator code improvements and n-gram seperation

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -1,78 +1,91 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed} from 'vue';
 
-// Define a reactive variable for the message input
 const message = ref('');
-const showKeywords = ref(false);
+const generatedKeywords = ref([]);
+const oneGrams = ref([]);
+const twoGrams = ref([]);
+const threeGrams = ref([]);
 
 const ngramGenerate = (keywords, n) => {
+  const res = [];
 
-    const res = [];
-
-    for(let i = 0; i <= keywords.length - n; i++) {
-
-        res.push(keywords.slice(i, i+n).join(" "));
-
-    }
-    return res;
+  //Finds all possible n-gram words
+  for(let i = 0; i <= keywords.length - n; i++) {
+      res.push(keywords.slice(i, i+n).join(" "));
+  }
+  return res;
 };
 
 const generateKeywords = () => {
     const inputText = message.value;
-    const keywords = inputText.split(/\s+/);
 
-    let resKeywords = [];
-    const oneGrams = ngramGenerate(keywords, 1);
-    resKeywords = [...oneGrams];
-    if(inputText.length > 1) {
-        const twoGrams = ngramGenerate(keywords, 2);
-        resKeywords = [...resKeywords, ...twoGrams];
-    }
-    if(inputText.length > 2) {
-        const threeGrams = ngramGenerate(keywords, 3);
-        resKeywords = [...resKeywords, ...threeGrams];
-    }
+    // Replaces non-alphanumeric characters and multiple spaces with a single space
+    const cleanedText = inputText.replace(/[^\w\s]+/g, ' ').trim();
 
-    // Display or process the generated keywords
-    showKeywords.value = true;
+    // Splits the cleaned text into single words based on whitespace
+    const keywords = cleanedText.split(/\s+/);
+
+    // Method call for each ngram
+    oneGrams.value = ngramGenerate(keywords, 1);
+    twoGrams.value = ngramGenerate(keywords, 2);
+    threeGrams.value = ngramGenerate(keywords, 3);
+
+    let resKeywords = [...oneGrams.value, ...twoGrams.value, ...threeGrams.value];
     return resKeywords;
 }
-
-const generatedKeywords = ref([]);
 
 const displayKeywords = () => {
   generatedKeywords.value = generateKeywords();
 };
+
+const showKeywords = computed(() => {
+    return generatedKeywords.value.length > 1;
+});
 </script>
 
 <template>
-    <div class="ma-keywords-generator">
-        <div class="ma-header">
-            <span>Keyword Generator</span>
-        </div>
-        <textarea v-model="message" id="keywordgen" name="keywordgen" rows="10" cols="50"
-                placeholder="Type your keywords here...">{{ message }}</textarea>
-        <button @click="displayKeywords" class="generate-button">Generate Keywords</button> 
+  <div class="ma-keywords-generator">
+      <div class="ma-header">
+          <span>Keyword Generator</span>
+      </div>
+      <textarea v-model="message" id="keywordgen" name="keywordgen" rows="10" cols="50"
+              placeholder="Type your keywords here...">{{ message }}</textarea>
+      <button @click="displayKeywords" class="generate-button">Generate Keywords</button> 
+  </div>
+  <div v-if="showKeywords">
+    <div class="ma-header">
+        <span>Generated Keywords</span>
     </div>
-    <div v-if="showKeywords">
-        <div class="ma-header">
-            <span>Generated Keywords</span>
-        </div>
-        <div class="keywords-container">
-            <ul id="keywords-list">
-            <li v-for="keyword in generatedKeywords" :key="keyword">{{ keyword }}</li>
-            </ul>
-        </div>
+    <div class="keywords-container">
+      <div class="column">
+          <div class="column-header">1-Grams</div>
+          <ul id="onegrams-list">
+              <li v-for="keyword in oneGrams" :key="keyword">{{ keyword }}</li>
+          </ul>
+      </div>
+      <div class="column">
+          <div class="column-header">2-Grams</div>
+          <ul id="twograms-list">
+              <li v-for="keyword in twoGrams" :key="keyword">{{ keyword }}</li>
+          </ul>
+      </div>
+      <div class="column">
+        <div class="column-header">3-Grams</div>
+        <ul id="threegrams-list">
+            <li v-for="keyword in threeGrams" :key="keyword">{{ keyword }}</li>
+        </ul>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
 
-  
   <style scoped>
   .ma-keywords-generator {
     display: flex;
     flex-direction: column;
-    align-items: center; /* Horizontally center items */
-    height: 40vh; /* Ensures full viewport height */
+    align-items: center;
+    height: 40vh;
   }
   
   .ma-header {
@@ -82,16 +95,15 @@ const displayKeywords = () => {
   }
   
   textarea {
-    width: 100%; /* Takes full width of its container */
-    max-width: 600px; /* Adjust maximum width as needed */
+    width: 100%;
+    max-width: 600px;
     padding: 10px;
     box-sizing: border-box;
     border: 1px solid #ccc;
     border-radius: 4px;
-    resize: vertical; /* Allow vertical resizing */
+    resize: vertical;
   }
 
-  
 .generate-button {
   margin-top: 1rem;
   padding: 10px 20px;
@@ -107,8 +119,9 @@ const displayKeywords = () => {
 }
 
 .keywords-container {
+  display: flex;
   width: 100%;
-  max-width: 800px; /* Adjust maximum width for keywords container */
+  max-width: 800px;
 }
 
 .keywords-container ul {
@@ -119,5 +132,16 @@ const displayKeywords = () => {
 .keywords-container li {
   margin-bottom: 5px;
 }
-  </style>
-  
+
+.column {
+    flex: 1;
+    margin: 0 10px;
+    text-align: center;
+    border: 1px solid #ccc;
+}
+
+.column-header {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+}
+</style>
