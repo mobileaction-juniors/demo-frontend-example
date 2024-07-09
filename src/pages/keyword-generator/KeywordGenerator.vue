ea7eacb1aa9c9e6182e9ee4ca791ccbba831de1c
Yagiz Basaran <bompowmsp@gmail.com>
2024-07-09 09:24:06 +0300

    fix: meaningful variable and function names, formatting, line reduction.

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -1,53 +1,53 @@
 <script setup>
+
 import { ref } from 'vue'
 
 const sentence = ref('')
-const maxN = ref(3)
+const N = ref(3)
 const combinations = ref([])
 
-const handle = (event) => {
-  event.preventDefault()
-
+const generateKeywords = () => {
   // Reset combinations
   combinations.value = []
 
   const words = sentence.value.split(/\s+/)
 
-  for (let n = 1; n <= maxN.value; n++) {
+  for (let i = 1; i <= N.value; i++) {
     const uniqueCombinations = new Set()
 
-    for (let i = 0; i <= words.length - n; i++) {
-      const combination = words.slice(i, i + n).join(' ')
+    for (let j = 0; j <= words.length - i; j++) {
+      const combination = words.slice(j, j + i).join(' ')
       uniqueCombinations.add(combination)
     }
 
-    if (uniqueCombinations.size != 0) {
-      combinations.value.push(Array.from(uniqueCombinations).join(", "))
+    if (uniqueCombinations.size) {
+      combinations.value.push(Array.from(uniqueCombinations).join(', '))
     }
   }
-}
+};
+
 </script>
 
 <template>
     <div class="ma-keywords-generator">
-        <form>
-            <div class="ma-header">
-                <label class="text-black-500 text-3xl" for="allwords">Keyword Generator</label>
+
+        <div class="ma-header">
+            <label class="text-black-500 text-3xl" for="allwords">Keyword Generator</label>
+        </div>
+        <div>
+            <textarea class="input-area" id="allwords" v-model="sentence"></textarea>
+        </div>
+        <div>
+            <button style="margin-top: 10px;" @click="generateKeywords">Submit</button>
+            <div v-for="(keyword, index) in combinations" :key="index" class="preview">
+                {{ keyword }}
             </div>
-            <div>
-                <textarea class="input-area" id="allwords" v-model="sentence"></textarea>
-            </div>
-            <div>
-                <button style="margin-top: 10px;" @click="handle">Submit</button>
-                <div v-for="(combo, index) in combinations" :key="index" class="preview">
-                    {{ combo }}
-                </div>
-            </div>
-        </form>
+        </div>
+
     </div>
 </template>
 
-<style>
+<style scoped>
 .ma-keywords-generator, .ma-header {
     display: grid;
     align-items: center;
@@ -57,9 +57,8 @@ const handle = (event) => {
 .input-area {
     height: 200px; 
     resize: none; 
-    border-color: cornflowerblue; 
+    border: 10px solid cornflowerblue;
     border-radius: 10px; 
-    border-width: 10px;
     margin: 5px;
 }
 .preview {
