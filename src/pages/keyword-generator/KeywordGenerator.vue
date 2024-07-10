bf3ca858bcf85858cda32d8881a81ebf3a75a631
Akif <akif.sahin@ug.bilkent.edu.tr>
2024-07-10 12:06:19 +0300

    feat: installed tailwindcss and converted to tailwindcss

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -2,7 +2,7 @@
 import {computed, ref} from 'vue'
 import {cleanDescription} from "@/utils/CleanDescription.js";
 import {filterArr} from "@/cleanupResources.js";
-import {MaBadge, MaInput} from "@mobileaction/action-kit";
+import {MaBadge, MaButton, MaCheckbox, MaInput} from "@mobileaction/action-kit";
 import "@mobileaction/action-kit/dist/style.css"
 
 const MAX_VALUE_OF_NGRAM_SIZE = 10;
@@ -13,7 +13,7 @@ const keywordText = ref('')
 const selectedNgramSizes = ref(Array(MAX_VALUE_OF_NGRAM_SIZE));
 
 const uncheckInvisibleCheckboxes = () => {
-  for ( let i = cleanKeywords.value.length; i < MAX_VALUE_OF_NGRAM_SIZE; i++ ) {
+  for (let i = cleanKeywords.value.length; i < MAX_VALUE_OF_NGRAM_SIZE; i++) {
     selectedNgramSizes.value[i] = false;
   }
 }
@@ -41,7 +41,7 @@ const cleanKeywords = computed(() => {
 const ngramSizes = computed(() => {
   const sizes = [];
   for (let i = 0; i < MAX_VALUE_OF_NGRAM_SIZE; i++) {
-    if (selectedNgramSizes.value[i] && i+1 <= cleanKeywords.value.length) {
+    if (selectedNgramSizes.value[i] && i + 1 <= cleanKeywords.value.length) {
       sizes.push(i + 1)
     }
   }
@@ -59,120 +59,39 @@ const ngramsList = computed(() => {
 </script>
 
 <template>
-  <div class="ma-keywords-generator">
-    <div class="ma-header">
-      <span>Keyword Generator</span>
-    </div>
-    <div class="ma-keyword-text-input-wrapper">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <div class="w-5/6 md:w-1/2 mx-auto">
+    <h3 class="font-sans font-bold text-center my-10 text-3xl">Keyword Generator</h3>
+    <div class="flex max-sm:flex-col md:flex-row">
       <ma-input
           type="textarea"
           placeholder="Enter the text to generate keywords from"
           rows=5
           size="large"
-          class="ma-keyword-textarea"
+          class="basis-1/2"
           v-model:value="keywordText"
           @change="uncheckInvisibleCheckboxes()"
       >
       </ma-input>
-    </div>
-    <div class="ma-keyword-ngram-size-selector-wrapper">
-      <div v-for="ngramSize in Math.min(MAX_VALUE_OF_NGRAM_SIZE, cleanKeywords.length)" v-bind:key="ngramSize" class="ma-keyword-ngram-size-selector"
-           @click="toggleNgramCheckbox(ngramSize)">
-        <input type="checkbox" :id="`ngram-${ngramSize}`" :value="ngramSize" v-model="selectedNgramSizes[ngramSize-1]"
-               @click.stop/>
-        <label :for="`ngram-${ngramSize}`" @click="toggleNgramCheckbox(ngramSize)">{{ ngramSize }}-gram</label>
+      <div class="grid grid-cols-4 gap-2 px-3">
+        <ma-checkbox v-for="ngramSize in Math.min(MAX_VALUE_OF_NGRAM_SIZE, cleanKeywords.length)"
+                   v-bind:key="ngramSize" @click="toggleNgramCheckbox(ngramSize)" > {{ ngramSize }}-gram </ma-checkbox>
       </div>
     </div>
-    <div class="ma-ngrams-wrapper">
-      <div v-for="ngramData in ngramsList" v-bind:key="ngramData" class="ma-ngrams-column">
-        <div class="ma-ngrams-column-title">
+  </div>
+  <div class="w-5/6 mx-auto mt-5">
+    <div class="flex flex-row flex-wrap justify-center my-3">
+      <div v-for="ngramData in ngramsList" v-bind:key="ngramData" class="shrink mx-2">
+        <div class="text-center font-bold">
           {{ ngramData.size }}-grams
         </div>
-        <div v-for="ngram in ngramData.grams.value" v-bind:key="ngram" class="ma-ngrams-ngram">
+        <div v-for="ngram in ngramData.grams.value" v-bind:key="ngram" class="my-3 flex flex-row flex-wrap justify-center">
           <ma-badge v-for="ngramWord in ngram.split(' ')" v-bind:key="ngramWord" variant="dark" size="large"
-                   type="primary" class="ma-ngrams-badge">
-            <div class="ma-ngrams-ngram-text">
+                    type="primary" class="text-center mr-0.5">
               {{ ngramWord }}
-            </div>
           </ma-badge>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-.ma-keywords-generator {
-  margin: 0 15%;
-}
-
-.ma-header {
-  text-align: center;
-  margin: 20px auto;
-  font-size: larger;
-  font-weight: bold;
-}
-
-.ma-keyword-ngram-size-selector-wrapper {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
-  gap: 10px;
-  align-items: center;
-}
-
-.ma-keyword-ngram-size-selector {
-  display: flex;
-  flex-direction: row;
-  background-color: #e3e3e3;
-  border-radius: 5px;
-  padding: 10px;
-}
-
-.ma-keyword-text-input-wrapper {
-  text-align: center;
-}
-
-.ma-keyword-textarea {
-  width: 100%;
-  margin: 10px auto;
-}
-
-.ma-ngrams-wrapper {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
-  gap: 10px;
-  margin: 10px auto 0;
-}
-
-.ma-ngrams-ngram {
-  margin: 7px 0;
-  text-align: center;
-}
-
-.ma-ngrams-badge {
-  margin: 2px;
-  padding: 7px;
-  display: inline-block;
-}
-
-.ma-ngrams-ngram-text {
-  font-size: small;
-  text-align: center;
-  margin: auto;
-}
-
-.ma-ngrams-column {
-  text-align: center;
-  background-color: #e3e3e3;
-  padding: 10px;
-  border-radius: 5px;
-  display: flex;
-  flex-direction: column;
-}
-
-.ma-ngrams-column-title {
-  text-align: center;
-  font-size: 15px;
-  font-weight: bold;
-}
-</style>
