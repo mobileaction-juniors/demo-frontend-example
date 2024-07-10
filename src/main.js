a8e6c3fbffad5392bfab148fb52cbb8cda59d0cd
Yagiz Basaran <bompowmsp@gmail.com>
2024-07-10 14:45:04 +0300

    feat(ONB-202): Improve quality of the user experience

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "@mobileaction/action-kit/dist/style.css"
+
 const app = createApp(App)
 
 app.use(router)
--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -1,27 +1,32 @@
 <script setup>
 import { ref } from 'vue'
+import { MaInput } from "@mobileaction/action-kit"
+import { MaBadge } from "@mobileaction/action-kit"
 
+const checkedNumbers = ref([])
+const excludeWords = ref('is, a, an, the') // default words to exclude
 const sentence = ref('')
-const MAX_N_GRAM_COUNT = 3
 const nGrams = ref([])
 
 const generateNGrams = () => {
-   nGrams.value = []
+    nGrams.value = []
+    checkedNumbers.value.sort((a, b) => a - b)
+    
+    const excludeWordsArray = excludeWords.value.split(/\s*,\s*/).map(word => word.toLowerCase())
+    const words = sentence.value.split(/\s+/).filter(word => !excludeWordsArray.includes(word.toLowerCase()))
 
-  const words = sentence.value.split(/\s+/)
+    checkedNumbers.value.forEach(n => {
+        const uniqueCombinations = new Set()
 
-  for (let i = 1; i <= MAX_N_GRAM_COUNT; i++) {
-    const uniqueCombinations = new Set()
+        for (let j = 0; j <= words.length - n; j++) {
+            const combination = words.slice(j, j + n).join(' ')
+            uniqueCombinations.add(combination)
+        }
 
-    for (let j = 0; j <= words.length - i; j++) {
-      const combination = words.slice(j, j + i).join(' ')
-      uniqueCombinations.add(combination)
-    }
-
-    if (uniqueCombinations.size) {
-        nGrams.value.push(Array.from(uniqueCombinations).join(', '))
-    }
-  }
+        if (uniqueCombinations.size) {
+            nGrams.value.push({ n, combinations: Array.from(uniqueCombinations) })
+        }
+    })
 };
 </script>
 
@@ -30,11 +35,22 @@ const generateNGrams = () => {
         <div class="ma-header">
             <label for="allwords">Keyword Generator</label>
         </div>
-        <textarea class = "input-area" id="allwords" v-model = "sentence"></textarea>
+        <MaInput id="allwords" v-model:value = "sentence" type="textarea" hint-text="Please select how you want to see n-grams and dont forget to use ',' for different exluded words." style="height: 100px; resize: none; border-radius: 10px; margin: 5px; width: 100%;"> </MaInput>
+        <div style="margin: 10px">Checked N-Grams: {{ checkedNumbers }}</div>
+        <div style="display: flex; margin: 10px">
+            <div v-for="index in 10" :key="index" >
+                <input type="checkbox" :id="'n' + index" :value="index" v-model="checkedNumbers" />
+                <label :for="'n' + index">{{ index }}</label>
+            </div>
+        </div>
+        <label for="excludeWords">Exclude Words:</label>
+        <input type="text" id="excludeWords" v-model="excludeWords" placeholder="Comma separated">
         <button class="submit-button" style="margin-top: 10px;" @click="generateNGrams">Submit</button>
         <div class="preview" v-for ="(keyword, index) in nGrams" :key="index" >
-            <strong>{{ index + 1 + "-gram:" }}</strong>
-            {{ keyword }}
+            <strong>{{ keyword.n + "-gram:" }}</strong>
+            <div v-for="tag in keyword.combinations" style="width: fit-content; margin: 2px;">
+                <ma-badge shape="square" variant="dark" type="secondary" size="medium"><ma-badge shape="dot" variant="dark"/> {{ tag }} </ma-badge>
+            </div>
         </div>
     </div>
 </template>
@@ -74,5 +90,6 @@ const generateNGrams = () => {
   padding: 10px 20px;
   margin: 5px 0;
   width: 50%;
+  
 }
 </style>
