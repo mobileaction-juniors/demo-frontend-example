fa37cfc1be8fca759a96637aeb85fdb666c885c1
Akif <akif.sahin@ug.bilkent.edu.tr>
2024-07-11 09:43:32 +0300

    refactor: made simplifications and converted inline tailwindcss styling to css classes using @apply directive

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -62,42 +62,42 @@ const ngramSizes = computed(() => {
 </script>
 
 <template>
-  <div class="w-5/6 mx-auto">
-    <div class="md:w-2/3 mx-auto">
-      <h3 class="font-sans font-bold text-center my-10 text-3xl">Keyword Generator</h3>
-      <div class="flex max-sm:flex-col md:flex-row">
+  <div class="kg-root">
+    <div class="kg-top">
+      <h3 class="kg-header">Keyword Generator</h3>
+      <div class="kg-input-wrapper">
         <ma-input
             type="textarea"
             placeholder="Enter the text to generate keywords from"
             rows=5
             size="large"
-            class="basis-1/2"
+            class="kg-input-text-area"
             v-model:value="keywordText"
             @change="uncheckInvisibleCheckboxes()"
         >
         </ma-input>
-        <div class="grid grid-cols-4 gap-2 px-3">
+        <div class="kg-input-checkboxes-wrapper">
           <ma-checkbox v-for="ngramSize in Math.min(MAX_VALUE_OF_NGRAM_SIZE, cleanKeywords.length)"
                        v-bind:key="ngramSize" @click="toggleNgramCheckbox(ngramSize)"> {{ ngramSize }}-gram
           </ma-checkbox>
         </div>
       </div>
+      <ma-button type="primary" variant="dark" class="kg-input-button" icon="add" @click="generateNGrams()">Generate Ngrams
+      </ma-button>
     </div>
-    <div class="md:w-2/3 mx-auto my-3">
-      <ma-button type="primary" variant="dark" class="" icon="add" @click="generateNGrams()">Generate Ngrams</ma-button>
-    </div>
-    <div class="mx-auto mt-5">
-      <div class="flex flex-row flex-wrap justify-center my-3">
-        <div v-for="ngramData in ngramsList" v-bind:key="ngramData" class="shrink mx-2">
-          <ma-badge variant="dark" size="large" type="primary" class="py-1">
-            <h1 class="font-bold text-lg mx-auto">
+    <div class="kg-ngrams-wrapper">
+      <div class="kg-ngrams-columns-wrapper">
+        <div v-for="ngramData in ngramsList" v-bind:key="ngramData" class="kg-ngrams-column">
+          <ma-badge variant="dark" size="large" type="primary" class="kg-ngrams-column-title-wrapper">
+            <h1 class="kg-ngrams-column-title">
               {{ ngramData.size }}-grams
             </h1>
           </ma-badge>
           <div v-for="ngram in ngramData.grams" v-bind:key="ngram"
-               class="group my-1 p-1 rounded-full flex flex-row flex-wrap justify-center hover:bg-gray-800">
+               class="group kg-ngrams-ngram">
             <ma-badge v-for="ngramWord in ngram" v-bind:key="ngramWord" variant="dark" size="large"
-                      type="primary" class="transition text-center mr-0.5 group-hover:bg-gray-100 group-hover:text-gray-800">
+                      type="primary"
+                      class="kg-ngrams-ngram-word">
               {{ ngramWord }}
             </ma-badge>
           </div>
@@ -106,3 +106,62 @@ const ngramSizes = computed(() => {
     </div>
   </div>
 </template>
+
+<style lang="postcss">
+.kg-root {
+  @apply w-5/6 mx-auto
+}
+
+.kg-top {
+  @apply md:w-2/3 mx-auto
+}
+
+.kg-header {
+  @apply font-sans font-bold text-center my-10 text-3xl
+}
+
+.kg-input-wrapper {
+  @apply flex max-sm:flex-col md:flex-row
+}
+
+.kg-input-text-area {
+  @apply basis-1/2
+}
+
+.kg-input-checkboxes-wrapper {
+  @apply grid grid-cols-4 gap-2 px-3
+}
+
+.kg-input-button {
+  @apply my-3
+}
+
+.kg-ngrams-wrapper {
+  @apply mx-auto mt-5
+}
+
+.kg-ngrams-columns-wrapper {
+  @apply flex flex-row flex-wrap justify-center my-3
+}
+
+.kg-ngrams-column {
+  @apply shrink mx-2
+}
+
+.kg-ngrams-column-title-wrapper {
+  @apply py-1
+}
+
+.kg-ngrams-column-title {
+  @apply font-bold text-lg mx-auto
+}
+
+.kg-ngrams-ngram {
+  @apply my-1 p-1 rounded-full flex flex-row flex-wrap justify-center hover:bg-gray-800
+}
+
+.kg-ngrams-ngram-word {
+  @apply transition text-center mr-0.5 group-hover:bg-gray-100 group-hover:text-gray-800
+}
+
+</style>
