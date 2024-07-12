68d0d011f2b12bcee78a4d69996036e8f9af325f
Yagiz Basaran <bompowmsp@gmail.com>
2024-07-12 12:27:27 +0300

    chore: Readability changes

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -39,30 +39,40 @@ const generateNGrams = () => {
 </script>
 
 <template>
-  <div class="ma-keywords-generator p-4">
-      <div class="ma-header mb-4">
-          <label for="allwords" class="text-xl font-bold">Keyword Generator</label>
-      </div>
-      <MaInput id="allwords" v-model:value="inputText" type="textarea" hint-text="Please select how you want to see n-grams and don't forget to use ',' for different excluded words." class="input-area resize-none w-full p-2 mb-1" style="resize: none;"></MaInput>
-      <div class="mb-4 text-sm">Checked N-Grams: {{ selectedNGrams }}</div>
-      <div class="flex flex-wrap mb-4 space-x-2">
-          <div v-for="index in Math.min(filteredWords.length, 10)" :key="index" class="flex items-center space-x-1">
-              <input type="checkbox" :id="'n' + index" :value="index" v-model="selectedNGrams" class="mr-1">
-              <label :for="'n' + index">{{ index }}</label>
-          </div>
-      </div>
-      <label for="excludeWords" class="block mb-2">Exclude Words:</label>
-      <input type="text" id="excludeWords" v-model="excludeWords" placeholder="Comma separated" class="w-full md:w-1/2 lg:w-1/3 p-2 border border-gray-300 rounded mb-4 transition-all duration-500 ease-in-out">
-      <MaButton class="submit-button mb-4 w-full md:w-auto transition-all duration-500 ease-in-out" type="primary" variant="dark" icon="tick" @click="generateNGrams">Submit</MaButton>
-      <div class="preview space-y-4" v-for="(keyword, index) in nGrams" :key="index">
-          <strong>{{ keyword.n + "-gram:" }}</strong>
-          <div class="flex flex-wrap">
-              <div v-for="tag in keyword.combinations" :key="tag" class="m-1">
-                  <MaBadge shape="square" variant="dark" type="secondary" size="medium">
-                      <MaBadge shape="dot" variant="dark"/> {{ tag }}
-                  </MaBadge>
-              </div>
-          </div>
-      </div>
-  </div>
+    <div class="ma-keywords-generator p-4">
+        <div class="ma-header mb-4">
+            <label for="allwords" class="text-xl font-bold">Keyword Generator</label>
+        </div>
+        <MaInput id="allwords" 
+                v-model:value="inputText" 
+                type="textarea" 
+                hint-text="Please select how you want to see n-grams and don't forget to use ',' for different excluded words." 
+                class="input-area resize-none w-full p-2 mb-1" 
+                style="resize: none;">
+        </MaInput>
+        <div class="mb-4 text-sm">Checked N-Grams: {{ selectedNGrams }}</div>
+        <div class="flex flex-wrap mb-4 space-x-2">
+            <div v-for="index in Math.min(filteredWords.length, 10)" :key="index" class="flex items-center space-x-1">
+                <input type="checkbox" :id="'n' + index" :value="index" v-model="selectedNGrams" class="mr-1">
+                <label :for="'n' + index">{{ index }}</label>
+            </div>
+        </div>
+        <label for="excludeWords" class="block mb-2">Exclude Words:</label>
+        <input type="text" id="excludeWords" v-model="excludeWords" placeholder="Comma separated" class="w-full md:w-1/2 lg:w-1/3 p-2 border border-gray-300 rounded mb-4 transition-all duration-500 ease-in-out">
+        <MaButton class="submit-button mb-4 w-full md:w-auto transition-all duration-500 ease-in-out" 
+                type="primary" 
+                variant="dark" 
+                icon="tick" 
+                @click="generateNGrams">Submit</MaButton>
+        <div class="preview space-y-4" v-for="(keyword, index) in nGrams" :key="index">
+            <strong>{{ keyword.n + "-gram:" }}</strong>
+            <div class="flex flex-wrap">
+                <div v-for="tag in keyword.combinations" :key="tag" class="m-1">
+                    <MaBadge shape="square" variant="dark" type="secondary" size="medium">
+                        <MaBadge shape="dot" variant="dark"/> {{ tag }}
+                    </MaBadge>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
