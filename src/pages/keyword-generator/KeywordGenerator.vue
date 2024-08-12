1274412e72695dcd70b178e91f9ddb29ae83be77
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-08-12 16:55:22 +0300

    ONB-204 new page is added in main page as a component. Most of the style is converted to tailwing css. Two pages are made responsive by using tailwind. All punctuations are erased in input area.

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -1,32 +1,41 @@
 <template>
-    <div class="p-5 max-w-[1000px] mx-auto mt-[10%]">
-        <div class="flex justify-around items-start">
-            <div class="w-[35%]">
-                <MaInput v-model:value="inputText" type="textarea" placeholder="Write your text here" @change="clearText" hint-text="Enter your filter words below."></MaInput>
-                <MaTagInput :tags="unwantedWords" size="sm" />
-            </div>
-            <div class="flex flex-col mx-auto" v-if="inputText !== '' ">
-                <MaButton @click="generateKeywords" size="medium" variant="filled" color="blak"> 
-                    <MaIcon name="chevrons-right" size="xl" /> 
-                </MaButton>
-                <MaSelect
-                    v-model:value="selectedNgram"
-                    placeholder="Select option..."
-                    :options="nGramData"
-                    size="small"
-                    allow-clear
-                    dropdown-match-select-width
-                    mode="multiselect">
-                </MaSelect>
-            </div>
-            <div v-if="inputText !== '' " class="w-[35%] max-h-[200px] overflow-y-auto p-2.5">
-                <div class="overflow-y-auto">
-                    <div v-for="(group, groupIndex) in keywordTags" :key="groupIndex">
-                        <h3>{{ group.label }}</h3> <!-- Display the n-gram group label -->
-                        <div v-for="(tag, index) in group.tags" :key="index" class="inline-block bg-gray-300 p-1.5 mx-1.5 my-1.5 rounded-full text-sm">
-                            {{ tag }}
+    <div v-if="page==='analysisPage'">
+        <KeywordAnalysisPage v-model:text="filteredText" @goToMainPage="goToMainPage" ></KeywordAnalysisPage>
+        
+    </div>
+    <div v-if="page==='indexPage'">
+        <div class="p-5 max-w-[1200px] mx-auto mt-[10%]">
+            <div class="flex flex-col mx-auto justify-center items-center md:flex-row md:justify-around md:items-start">
+                <div class="w-[90%] md:w-[35%]">
+                    <MaInput v-model:value="inputText" type="textarea" placeholder="Write your text here" @change="clearText" hint-text="Enter your filter words below."></MaInput>
+                    <MaTagInput :tags="unwantedWords" size="sm" />
+                </div>
+                <div class="flex flex-col items-center mt-[5%] mb-[5%] md:flex-row md:items-start" v-if="inputText !== '' ">
+                    <MaButton @click="generateKeywords" size="medium" variant="filled" color="black"> 
+                        <MaIcon name="chevrons-right" size="xl" /> 
+                    </MaButton>
+                    <MaSelect
+                        v-model:value="selectedNgram"
+                        placeholder="Select option..."
+                        :options="nGramData"
+                        size="small"
+                        allow-clear
+                        dropdown-match-select-width
+                        mode="multiselect">
+                    </MaSelect>
+                </div>
+                <div v-if="inputText !== '' " class="w-full md:w-[35%] relative text-center md:text-left">
+                    <div class="w-[100%] max-h-[200px] overflow-y-auto p-2.5">
+                        <div v-for="(group, groupIndex) in keywordTags" :key="groupIndex">
+                            <h3>{{ group.label }}</h3>
+                            <div v-for="(tag, index) in group.tags" :key="index" class="inline-block bg-gray-300 p-1.5 mx-1.5 my-1.5 rounded-full text-sm">
+                                {{ tag }}
+                            </div>
+                            
                         </div>
+                        
                     </div>
+                    <MaButton class="absolute mt-5 " v-if="keywordTags.length!==0" @click="page = 'analysisPage'">Go to Analysis</MaButton>
                 </div>
             </div>
         </div>
@@ -35,10 +44,12 @@
 
 <script>
 import { MaButton, MaIcon, MaInput, MaSelect, MaTagInput } from "@mobileaction/action-kit"
+import KeywordAnalysisPage from "./KeywordAnalysisPage.vue";
 export default {
     name: 'KeywordGenerator',
     data() {
         return {
+            page:"indexPage",
             inputText: "",
             selectedNgram: [],
             nGramRange: 10,
@@ -56,7 +67,8 @@ export default {
                 {value:9,label:"9-gram"},
                 {value:10,label:"10-gram"},
             ],
-            keywordTags: []
+            keywordTags: [],
+            filteredText:""
         };
     },
     components:{
@@ -64,15 +76,16 @@ export default {
         MaIcon,
         MaInput,
         MaSelect,
-        MaTagInput
+        MaTagInput,
+        KeywordAnalysisPage
     },
     methods: {
         generateKeywords() {
             this.keywordTags = [];
-            const text = this.inputText.trim().toLowerCase();
+            const text = this.inputText.replace(/[".,/#!$%^&*;:{}=\-_`~()]/g, "").trim().toLowerCase();
             let words = text.split(" ");
             words = words.filter(word => !this.unwantedWords.includes(word));
-            
+            this.filteredText = words.join(" ");
             this.selectedNgram.forEach(selectedGram => {
                 let nGramSet = new Set();
                 for (let i = 0; i <= words.length - selectedGram; i++) {
@@ -83,10 +96,12 @@ export default {
             });
         },
         clearText(){
-            console.log("Hi asdas")
             if(this.inputText===""){
                 this.outputText="";
             }
+        },
+        goToMainPage() {
+            this.page = 'indexPage';
         }
     }
 }
