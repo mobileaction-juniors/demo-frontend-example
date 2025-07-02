e5868bf5b6184d8362add5f74b1a9d7aecd2167e
mehmetserhatcelik <mehmetserhat86895@gmail.com>
2025-07-02 10:18:55 +0300

    fix(ONB-201): add redirect button to landing page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,16 @@
 <script setup>
+import Button from '@/components/Button.vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter();
+function goToKeywordGenerator() {
+  router.push('/keyword-generator');
+}
 </script>
 <template>
     <div style="text-align: center">
         <h1>Welcome to Onboarding Progress </h1>
         <p>You can see the your first task details via link. Please continue that steps while coding. </p>
-        <a href="https://mobileaction.atlassian.net/l/cp/7fw8MroM" target="_blank"> Implement First Front-End Task </a>
+        <Button label="Go to Keyword Generator" @click="goToKeywordGenerator" />
     </div>
 </template>
