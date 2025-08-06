93988f88b5a6deaa4c3e00fe5761ce21719c5535
Alperen Ovak <alperen.ovak@metu.edu.tr>
2025-08-06 12:09:19 +0300

    feat(ONB-202): Improve quality of the user experience

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -5,9 +5,41 @@
     <div class="input-group">
       <div class="input-column">
         <p><strong>Message:</strong> {{ inputText }}</p>
-        <input v-model="inputText" placeholder="n-gram me" class="input-box" />
+        <MaInput
+            v-model:value="inputText"
+            type="textarea"
+            title="Input Title"
+            size="large"
+            placeholder="n-gram me"
+            hintText="Write your text to create ngrams"
+        />
       </div>
     </div>
+
+    <MaCard class="ma-card" title="N-gram Selection" description="Choose n-gram types to generate (1-10)">
+      <div class="ma-ngram-selection-container">
+        <MaSelect
+            allowClear
+            showSearch = "false"
+            checkboxPlacement="left"
+            dropdownMatchSelectWidth
+            mode="multiselect"
+            multiSelectOptionType="toggle"
+            optionFilterProp="label"
+            optionItemRounded
+            :options="generateNgramRange(MAX_N)"
+            placeholder="Select option..."
+            size="small"
+            v-model:value="selectedNValues"
+        >
+        </MaSelect>
+        <MaCheckboxCard
+            v-model:checked="clearUnwantedSelected"
+        >
+            <template #title>Remove Stop Words</template>
+        </MaCheckboxCard>
+      </div>
+    </MaCard>
 
     <button @click="generateNGrams" class="generate-button">
       Generate N-Grams...
@@ -18,15 +50,15 @@
         :key="i"
         class="ngram-row"
     >
-      <strong class="ngram-title">{{ i + 1 }}-Gram :</strong>
+      <strong class="ngram-title">{{ words.label }} :</strong>
       <div class="word-list">
-        <span
-            v-for="(word, j) in words"
+        <MaBadge
+            v-for="(word, j) in words.value"
             :key="j"
             class="word-tag"
         >
           {{ word }}
-        </span>
+        </MaBadge>
       </div>
     </div>
   </div>
@@ -34,11 +66,26 @@
 
 <script setup>
 import { ref } from 'vue'
-import {regex} from "@/cleanupResources.js";
+import {cleanDescription, cleanUnwantedWords} from "@/utils/CleanDescription.js";
+import { MaSelect as MaSelect, MaCard, MaCheckbox2Card as MaCheckboxCard, MaInput, MaBadge } from '@mobileaction/action-kit'
+
+const MAX_N = 10;
+const DEF_N = 3;
 
 const inputText = ref('');
 const resultGrams = ref([]);
-const MAX_N = 3;
+const selectedNValues = ref(Array.from({ length: DEF_N }, (_, i) => i + 1));
+const clearUnwantedSelected = ref(false);
+
+function generateNgramRange(max = 10, start = 1) {
+  return Array.from({ length: max - start + 1 }, (_, i) => {
+    const value = i + start;
+    return {
+      label: `${value}-gram`,
+      value: value
+    };
+  });
+}
 
 function generateNGrams(){
 
@@ -47,11 +94,16 @@ function generateNGrams(){
   }
 
   resultGrams.value = [];
-  let cleanedText = getCleanedWords(inputText.value)
+  let cleanedText = cleanDescription(inputText.value).split(/\s+/);
+  if(clearUnwantedSelected.value){
+    cleanedText = cleanUnwantedWords(cleanedText);
+  }
+
   let wordCount = cleanedText.length;
 
+  const sorted = [...selectedNValues.value].sort((a, b) => a - b);
   //sliding window
-  for(let n=1; n<= MAX_N; n++){
+  for(let n of sorted){
     const window = [];
     let resultN = new Set();
 
@@ -64,21 +116,16 @@ function generateNGrams(){
     }
 
     if(resultN.size > 0){
-      resultGrams.value.push(Array.from(resultN));
+      resultGrams.value.push({
+            label: `${n}-Gram`,
+            value: Array.from(resultN)
+          }
+      );
     }
   }
 }
 
-function getCleanedWords(text){
-  return text
-      .toLowerCase()
-      .replace(regex,'')
-      .trim()
-      .split(/\s+/);
-}
-
 </script>
-
 
 <style>
 .container {
@@ -92,28 +139,6 @@ function getCleanedWords(text){
 .title {
   font-size: 2em;
   margin-bottom: 20px;
-}
-
-.input-group {
-  display: flex;
-  justify-content: center;
-  gap: 30px;
-  margin-bottom: 20px;
-}
-
-.input-column {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-}
-
-.input-box {
-  padding: 10px;
-  font-size: 1em;
-  border-radius: 6px;
-  border: 1px solid #ccc;
-  width: 100%;
 }
 
 .generate-button {
@@ -154,10 +179,24 @@ function getCleanedWords(text){
   gap: 8px;
 }
 
+.ma-ngram-selection-container{
+  display: flex;
+  justify-content: space-around;
+}
+
+.ma-card{
+  text-align: center;
+  justify-content: space-around;
+}
+
+.ak-card__header{
+  justify-self:center;
+}
+
 .word-tag {
   padding: 6px 10px;
-  background-color: #e0e0e0;
-  border-radius: 4px;
+  background-color: #0056b3;
+  border-radius: 6px;
   font-size: 0.95em;
 }
 </style>
--- src/utils/CleanDescription.js
+++ src/utils/CleanDescription.js
@@ -1,5 +1,10 @@
-import { regex,splitRegex } from '../cleanupResources';
+import { regex,splitRegex, filterArr } from '../cleanupResources';
 
 export function cleanDescription(description) {
     return description.replace(regex, '').replace(splitRegex, ' ').trim().replaceAll('İ', 'i').toLowerCase();
+}
+
+export function cleanUnwantedWords(input){
+    const filterSet = new Set(filterArr.map(word => word.toLowerCase()));
+    return input.filter(word => !filterSet.has(word.toLowerCase()));
 }
