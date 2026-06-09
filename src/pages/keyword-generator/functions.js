5489516572a2f3bb45b2a5f619d9cb68da3f3777
Cemil <cemil.canbaz@hotmail.com>
2026-06-09 15:08:25 +0300

    feat(ONB-202): Keyword generator page logic improved

--- src/pages/keyword-generator/functions.js
+++ src/pages/keyword-generator/functions.js
@@ -1,35 +1,22 @@
-const STOP_WORDS = new Set([
-    "an",
-    "a",
-    "is",
-    "are",
-    "was",
-    "were",
-    "of",
-    "the"
-])
+import { cleanDescription } from '../../utils/CleanDescription';
+import { filterArr } from '../../cleanupResources';
+
+const STOP_WORDS = new Set(filterArr);
 
 export const generateKeyword = (text, selectedNgrams) => {
+    const cleaned = cleanDescription(text)
+        .split(' ')
+        .filter(word => word && !STOP_WORDS.has(word))
+        .join(' ');
 
-    text = cleanText(text);
-    let textArray = text.split(' ');
-
+    const textArray = cleaned.split(' ');
     const result = [];
 
-    for(const ngram of selectedNgrams){
-        result.push({ngram: ngram, keywords: generateNGram(textArray, ngram)});
+    for (const ngram of selectedNgrams) {
+        result.push({ ngram, keywords: generateNGram(textArray, ngram) });
     }
 
     return result;
-}
-
-const cleanText = (text) => {
-
-    const regex = /\s+/g;
-    return text.replace(/\s+/g, ' ').trim()
-                                    .split(' ')
-                                    .filter(word => !STOP_WORDS.has(word.toLowerCase()))
-                                    .join(' ');
 }
 
 const generateNGram = (array, ngram) => {
