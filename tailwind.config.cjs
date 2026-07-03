193f6eb757ae3abd055eb10814707ad2926312a3
Emir Aydın <emiraydn2001@gmail.com>
2026-07-03 15:44:53 +0300

    chore: configure tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
