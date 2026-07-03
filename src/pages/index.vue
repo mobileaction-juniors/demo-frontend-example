63767dadec5c042789e090e6b7fd6b715c8b7680
Emir Aydın <emiraydn2001@gmail.com>
2026-07-03 15:45:04 +0300

    refactor: replace basic styles with tailwind classes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 
 </script>
 <template>
-    <div style="text-align: center">
+    <div class="text-center">
         <h1>Welcome to Onboarding Progress </h1>
         <p>You can see the your first task details via link. Please continue that steps while coding. </p>
         <a href="https://mobileaction.atlassian.net/l/cp/7fw8MroM" target="_blank"> Implement First Front-End Task </a>
