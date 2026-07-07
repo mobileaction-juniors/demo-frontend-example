36f214dc4a10fe3f0f100eb5563538cd77e0cc7e
Emir Aydın <emiraydn2001@gmail.com>
2026-07-07 14:11:00 +0300

    chore(app): add pinia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { ActionKit } from '@mobileaction/action-kit'
 import '@mobileaction/action-kit/dist/style.css'
 import './assets/tailwind.css'
@@ -6,7 +7,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(ActionKit)
 app.use(router)
 
