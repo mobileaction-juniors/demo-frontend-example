62b60fe292bf5c0ab6091b63b1898fd5e537e03d
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-09 11:51:52 +0300

    feat(keyword-generator): display generated keyword by n-gram

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -9,7 +9,7 @@ const cleanedInput = computed(() => cleanInput(userInput.value))
 const results = computed(() => {
   const ngrams = [];
   for (let i = 0; i < ngramLimit; i++) {
-    ngrams.push(generateNGram(cleanedInput, i + 1))
+    ngrams.push(generateNGram(cleanedInput.value, i + 1))
   }
   return ngrams;
 })
@@ -23,5 +23,14 @@ const results = computed(() => {
     <input v-model="userInput" placeholder="Type Something">
     <p>Cleaned User Input: {{cleanedInput}}</p>
     <hr>
+    //Displaying the generated keywords
+    <div>
+      <ul v-for="(nGram, index) in results" :key="index">
+        {{`${index + 1}-Gram`}}
+        <li v-for="(keyword, index) in nGram" :key="index">
+          {{keyword}}
+        </li>
+      </ul>
+    </div>
 
 </template>
