e9e94b41342899d9556c566b0c5064b029c727b8
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-09 17:49:32 +0300

    fix(keyword-generator): add new if to check cleanedKeywords.length less than size of grop n-gram

--- src/utils/GenerateNGram.js
+++ src/utils/GenerateNGram.js
@@ -4,6 +4,7 @@ export function generateNGram(input, n) {
     if (!input || input.length < n) return []
     const keywords = input.split(' ')
     const cleanedKeywords = keywords.filter(keyword => !stopWords.has(keyword))
+    if (cleanedKeywords.length < n) return []
     const nGrams = new Set() //to remove duplicates
     for (let i = 0; i < cleanedKeywords.length - (n - 1); i++) {
         nGrams.add(cleanedKeywords.slice(i, i + n).join(' '))
