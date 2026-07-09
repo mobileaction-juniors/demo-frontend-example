c1fec7771efdb9bed78fdb9bced5abb1aef4ae2c
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-09 14:09:00 +0300

    feat(generate-keyword): add filter to clean unwanted words

--- src/utils/GenerateNGram.js
+++ src/utils/GenerateNGram.js
@@ -1,9 +1,12 @@
+import {stopWords} from "@/utils/StopWords.js";
+
 export function generateNGram(input, n) {
     if (!input || input.length < n) return []
     const keywords = input.split(' ')
+    const cleanedKeywords = keywords.filter(keyword => !stopWords.includes(keyword))
     const nGrams = new Set() //to remove duplicates
-    for (let i = 0; i < keywords.length - (n - 1); i++) {
-        nGrams.add(keywords.slice(i, i + n).join(' '))
+    for (let i = 0; i < cleanedKeywords.length - (n - 1); i++) {
+        nGrams.add(cleanedKeywords.slice(i, i + n).join(' '))
     }
     return [...nGrams]
 }
