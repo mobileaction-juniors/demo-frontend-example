47cc12334097aea4af5b82bb301acf56e94eaeb2
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-10 10:36:33 +0300

    feat(keyword-generator): add new component for keyword density analyze

---
+++ src/components/KeywordDensity.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import {ref} from "vue";
+
+const props = defineProps({
+  userInput: {
+    type: String,
+    required: true
+  }
+})
+
+const inputText = ref(props.userInput)
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
