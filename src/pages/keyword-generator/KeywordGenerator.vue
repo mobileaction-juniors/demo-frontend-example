c1333d5a44d7438faf4532ae1059e17f5918b8c4
Emre <emreeoruc04@gmail.com>
2026-07-13 13:34:06 +0300

    feat(keyword-generator): add input box and implement cleaning

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { cleanDescription } from "../../utils/CleanDescription";
+import { ref, computed } from "vue";
+const description = ref("");
+const cleanedDescription = computed(() => cleanDescription(description.value));
 </script>
 <template>
-    <div class="ma-keywords-generator">
-        <div class="ma-header">
-            <span>Keyword Generator</span>
-        </div>
+  <div class="ma-keywords-generator">
+    <div class="ma-header">
+      <span>Keyword Generator</span>
     </div>
+    <div class="ma-text-area">
+      <textarea
+        v-model="description"
+        placeholder="Enter your description here..."
+        rows="8"
+      ></textarea>
+      <p v-if="description">{{ cleanedDescription }}</p>
+    </div>
+  </div>
 </template>
