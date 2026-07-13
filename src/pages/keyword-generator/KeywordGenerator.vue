b78c73e2431fedc7834a55a0541cd096a997d740
Emre <emreeoruc04@gmail.com>
2026-07-13 14:33:51 +0300

    feat(keyword-generator): add 1-gram, 2-gram, 3-gram generators to KeywordGenerator page

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -1,8 +1,24 @@
 <script setup>
 import { cleanDescription } from "../../utils/CleanDescription";
+import {
+  oneGramGeneration,
+  twoGramGeneration,
+  threeGramGeneration,
+} from "../../utils/nGramGeneration";
+
 import { ref, computed } from "vue";
 const description = ref("");
 const cleanedDescription = computed(() => cleanDescription(description.value));
+
+const oneGramKeywords = computed(() =>
+  oneGramGeneration(cleanedDescription.value),
+);
+const twoGramKeywords = computed(() =>
+  twoGramGeneration(cleanedDescription.value),
+);
+const threeGramKeywords = computed(() =>
+  threeGramGeneration(cleanedDescription.value),
+);
 </script>
 <template>
   <div class="ma-keywords-generator">
@@ -15,7 +31,27 @@ const cleanedDescription = computed(() => cleanDescription(description.value));
         placeholder="Enter your description here..."
         rows="8"
       ></textarea>
-      <p v-if="description">{{ cleanedDescription }}</p>
+      <p v-if="description">Cleaned Description: {{ cleanedDescription }}</p>
+    </div>
+    <div class="ma-keywords">
+      <div class="ma-keywords-section">
+        <h3>1-Gram Keywords</h3>
+        <p v-if="oneGramKeywords.length > 0">
+          {{ oneGramKeywords.join(", ") }}
+        </p>
+      </div>
+      <div class="ma-keywords-section">
+        <h3>2-Gram Keywords</h3>
+        <p v-if="twoGramKeywords.length > 0">
+          {{ twoGramKeywords.join(", ") }}
+        </p>
+      </div>
+      <div class="ma-keywords-section">
+        <h3>3-Gram Keywords</h3>
+        <p v-if="threeGramKeywords.length > 0">
+          {{ threeGramKeywords.join(", ") }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
