7a444a8a1493e3916a65a52d49fb1e441497b5e8
Emre <emreeoruc04@gmail.com>
2026-07-14 10:10:58 +0300

    feat(keyword-section): add keywordsection component to scale KeywordGenerator page

---
+++ src/components/KeywordsSection.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  keywords: {
+    type: Array,
+    default: () => [],
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="ma-keywords-section">
+    <h3>{{ title }}</h3>
+    <p v-if="keywords.length > 0">{{ keywords.join(", ") }}</p>
+  </div>
+</template>
--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -3,15 +3,18 @@ import { computed, ref } from "vue";
 
 import { cleanDescription } from "../../utils/CleanDescription";
 import { nGramGenerater } from "../../utils/nGramGeneration";
+import KeywordsSection from "@/components/KeywordsSection.vue";
 
 const description = ref("");
 const cleanedDescription = computed(() => cleanDescription(description.value));
 </script>
 <template>
   <div class="ma-keywords-generator">
+
     <div class="ma-header">
       <span>Keyword Generator</span>
     </div>
+    
     <div class="ma-text-area">
       <textarea
         v-model="description"
@@ -20,25 +23,21 @@ const cleanedDescription = computed(() => cleanDescription(description.value));
       ></textarea>
       <p v-if="description">Cleaned Description: {{ cleanedDescription }}</p>
     </div>
-    <div class="ma-keywords">
-      <div class="ma-keywords-section">
-        <h3>1-Gram Keywords</h3>
-        <p v-if="nGramGenerater(cleanedDescription, 1).length > 0">
-          {{ nGramGenerater(cleanedDescription, 1).join(", ") }}
-        </p>
-      </div>
-      <div class="ma-keywords-section">
-        <h3>2-Gram Keywords</h3>
-        <p v-if="nGramGenerater(cleanedDescription, 2).length > 0">
-          {{ nGramGenerater(cleanedDescription, 2).join(", ") }}
-        </p>
-      </div>
-      <div class="ma-keywords-section">
-        <h3>3-Gram Keywords</h3>
-        <p v-if="nGramGenerater(cleanedDescription, 3).length > 0">
-          {{ nGramGenerater(cleanedDescription, 3).join(", ") }}
-        </p>
-      </div>
+
+    <div class="ma-keywords-section">
+      <KeywordsSection
+        :title="'1-Gram Keywords'"
+        :keywords="nGramGenerater(cleanedDescription, 1)"
+      ></KeywordsSection>
+      <KeywordsSection
+        :title="'2-Gram Keywords'"
+        :keywords="nGramGenerater(cleanedDescription, 2)"
+      ></KeywordsSection>
+      <KeywordsSection
+        :title="'3-Gram Keywords'"
+        :keywords="nGramGenerater(cleanedDescription, 3)"
+      ></KeywordsSection>
     </div>
+
   </div>
 </template>
