51a9cf5f2d9859b15102801d1ef9489aab72c523
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-16 10:41:03 +0300

    fix(keyword-generator): redesign defineStore for useUserInputStore

--- src/stores/UserInput.js
+++ src/stores/UserInput.js
@@ -1,7 +1,11 @@
 import {defineStore} from 'pinia'
 import {ref} from "vue";
 
-export const useUserInputStore =
-    defineStore('userInput', () => ({
-    userInput: ref('')
-}))
+export const useUserInputStore = defineStore('userInput', () => {
+        const userInput = ref('')
+
+        return {
+            userInput,
+        }
+    }
+)
