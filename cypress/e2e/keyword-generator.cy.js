51dee39af9dc79cbb366858f77611a9705772b50
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-16 12:07:47 +0300

    test(keyword-generator) add new test into keyword generator for testing filters generated keywords by n-gram multi-select

--- cypress/e2e/keyword-generator.cy.js
+++ cypress/e2e/keyword-generator.cy.js
@@ -11,11 +11,37 @@ describe('Keyword Generator', () => {
             .type(inputText)
 
         cy.contains('Generate').click()
+
         cy.contains('Select option...').click()
         cy.contains('1-Gram').should('be.visible').click()
+
         cy.get('body').click(0, 0)
+
         cy.contains('quick').should('be.visible')
         cy.contains('brown').should('be.visible')
         cy.contains('fox').should('be.visible')
+    })
+
+    it('filters generated keywords by n-gram multi-select', () => {
+        const inputText = 'Quick brown fox jump over fox'
+
+        cy.get('input[placeholder="Enter text..."]')
+            .should('be.visible')
+            .type(inputText)
+
+        cy.contains('Generate').click()
+
+        cy.contains('Select option...').click()
+        cy.contains('1-Gram').should('be.visible').click()
+        cy.contains('3-Gram').should('be.visible').click()
+        cy.contains('5-Gram').should('be.visible').click()
+        cy.get('body').click(0, 0)
+
+        cy.contains('Generate').click()
+
+        cy.contains('1-Gram:').should('be.visible')
+        cy.contains('3-Gram:').should('be.visible')
+        cy.contains('5-Gram:').should('be.visible')
+        cy.contains('4-Gram').should('not.be.visible')
     })
 })
