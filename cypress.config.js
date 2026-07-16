e49ec6dd50f5961cda875788ab1e0734455d8ea9
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-16 11:58:57 +0300

    test(keyword-generator) add keyword generator e2e text for testing enters text and generates keyword

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 import {defineConfig} from "cypress";
 
 export default defineConfig({
+    allowCypressEnv: false,
     e2e: {
         baseUrl: 'http://localhost:5173/',
+        supportFile: false,
     }
 })
---
+++ cypress/e2e/keyword-generator.cy.js
@@ -0,0 +1,21 @@
+describe('Keyword Generator', () => {
+    beforeEach(() => {
+        cy.visit('/keyword-generator')
+    })
+
+    it('enters text and generates keywords on button click', () => {
+        const inputText = 'Quick brown fox jump over fox'
+
+        cy.get('input[placeholder="Enter text..."]')
+            .should('be.visible')
+            .type(inputText)
+
+        cy.contains('Generate').click()
+        cy.contains('Select option...').click()
+        cy.contains('1-Gram').should('be.visible').click()
+        cy.get('body').click(0, 0)
+        cy.contains('quick').should('be.visible')
+        cy.contains('brown').should('be.visible')
+        cy.contains('fox').should('be.visible')
+    })
+})
