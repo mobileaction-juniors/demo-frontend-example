10c91bec191b8d8fb8dd5b044d6702b01694d632
Emre <emreeoruc04@gmail.com>
2026-07-17 11:45:56 +0300

    configure browser years

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    legacy(),
   ],
   resolve: {
     alias: {
