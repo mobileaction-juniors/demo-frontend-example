3e24bff8ebab90074f05478dd790d02854056759
Emre <emreeoruc04@gmail.com>
2026-07-17 13:57:56 +0300

    fix(keyword-generator): delete redundant packages from congigs

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
-    legacy(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
