bddc2b7d54c035beeee811007ec82bb14d766b75
Emre <emreeoruc04@gmail.com>
2026-07-20 11:11:43 +0300

    update(keyword-generator): improve on the layout of page.

--- src/pages/keyword-generator/KeywordGenerator.vue
+++ src/pages/keyword-generator/KeywordGenerator.vue
@@ -31,22 +31,24 @@ const sections = computed(() =>
 </script>
 <template>
   <div>
-    <div class="p-4 m-4 text-3xl text-[#1bcf6c] font-bold flex justify-center items-center">
+    <div class="p-2 m-2 text-3xl text-[#1bcf6c] font-bold flex justify-center items-center">
       <h1>
         Generator
       </h1>
     </div>
     <div class="flex flex-col justify-center items-center">
-      <div class="p-4 m-4 flex flex-col items-center">
+      <div class="p-2 m-2 flex flex-col items-center">
         <MaTextarea
           v-model="description"
           placeholder="Enter your description here..."
           rows="10"
           class="w-96">
         </MaTextarea>
-        <p v-if="buttonCLicked">Cleaned and Filtered Description: {{ cleanedAndFilteredDescription }}</p>
+        <p :class="{invisible: !buttonCLicked}">{{ "Cleaned and Filtered Description: " + cleanedAndFilteredDescription}}</p>
       </div>
-      <div class="p-4 m-4 flex flex-row items-center gap-4">
+    </div>
+    <div class="p-2 m-2 flex flex-col items-center">
+      <div class="p-2 m-2 flex flex-row items-center gap-4">
         <MaButton
           color="green"
           icon="coffee-bulk"
@@ -58,23 +60,25 @@ const sections = computed(() =>
             v-model:value="selectedGramSizes"
             mode="multiple"
             :options="gramSizeOptions"
-            size="large"
+            size="small"
             placeholder="Select n-gram sizes"
             class="w-64"
           >
         </MaSelect2>
-      </div>
-    </div>
-    <div class="p-4 m-4 flex flex-col items-center">
-      <div v-if="buttonCLicked">
+      </div>      
+      <div :class="{ invisible: !buttonCLicked }">
         <KeywordsSection
           v-for="section in sections"
           :key="section.title"
           :title="section.title"
           :keywords="section.keywords"
-        ></KeywordsSection>        
+        >
+        </KeywordsSection>
       </div>
     </div>
   </div>
+    <!-- <div class="p-4 m-4 flex flex-col items-center"> -->
+
+  <!-- </div> -->
 </template>
 
