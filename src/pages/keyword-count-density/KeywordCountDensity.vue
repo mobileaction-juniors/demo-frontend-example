9b161bd619de035110e73f1777e8c24903e77bf7
Emre <emreeoruc04@gmail.com>
2026-07-21 12:09:17 +0300

    install ag-grid-community & feat(keyword-count-density): add grid

--- src/pages/keyword-count-density/KeywordCountDensity.vue
+++ src/pages/keyword-count-density/KeywordCountDensity.vue
@@ -1,5 +1,12 @@
 <script setup>
+
+import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
+import {AgGridVue} from "ag-grid-vue3";
+
 import { calculateKeywordDensity } from "@/utils/KeywordDensity.js";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
 const props = defineProps({
   text: {
     type: String,
@@ -7,11 +14,23 @@ const props = defineProps({
   },
 });
 
+const colData = [
+  {field: "keyword", flex : 1},
+  {field: "count", flex : 1},
+  {field: "density", flex : 1}
+];
+// const defaultColDef = ref({ flex: 1 });
+
 const keywordDensity = calculateKeywordDensity(props.text);
+
+const rowData = [];
+for (const { word, count, density } of keywordDensity) {
+  rowData.push({ keyword: word, count: count, density:density });
+}
 
 </script>
 <template>
-<div class="p-4 m-4">
-    
+<div class="p-4 m-4" style="height: 500px">
+  <AgGridVue :column-defs="colData" :row-data="rowData" style="height: 100%; width: 75%;" />
 </div>
 </template>
