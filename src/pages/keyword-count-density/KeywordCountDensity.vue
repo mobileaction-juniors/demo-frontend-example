cfe487fffbc4a63a3f95d547f659e21a4c8857ab
Emre <emreeoruc04@gmail.com>
2026-07-21 10:12:53 +0300

    feat(keyword-count-density): add calculateKeywordDensity utility function

--- src/pages/keyword-count-density/KeywordCountDensity.vue
+++ src/pages/keyword-count-density/KeywordCountDensity.vue
@@ -1,10 +1,14 @@
 <script setup>
-defineProps({
+import { calculateKeywordDensity } from "@/utils/KeywordDensity.js";
+const props = defineProps({
   text: {
     type: String,
     required: true,
   },
 });
+
+const keywordDensity = calculateKeywordDensity(props.text);
+
 </script>
 <template>
 <div class="p-4 m-4">
