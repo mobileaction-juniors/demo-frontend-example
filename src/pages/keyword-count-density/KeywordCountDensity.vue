b08d447223cd52fa342c4c2fdabdc5ec867b3737
Emre <emreeoruc04@gmail.com>
2026-07-21 14:27:38 +0300

    feat(keyword-count-density): add text area & add button

--- src/pages/keyword-count-density/KeywordCountDensity.vue
+++ src/pages/keyword-count-density/KeywordCountDensity.vue
@@ -2,6 +2,8 @@
 
 import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
 import {AgGridVue} from "ag-grid-vue3";
+import {MaButton, MaTextarea} from "@mobileaction/action-kit";
+import { ref } from "vue";
 
 import { calculateKeywordDensity } from "@/utils/KeywordDensity.js";
 
@@ -14,23 +16,47 @@ const props = defineProps({
   },
 });
 
+function putRowData(text){
+  const rowData = [];
+  const keywordDensity = calculateKeywordDensity(text);
+  for (const { word, count, density } of keywordDensity) {
+    rowData.push({ keyword: word, count: count, density:density });
+  }
+  return rowData;
+}
+
+function handleButtonClick() {
+  rowData.value = putRowData(newText.value);
+}
+
 const colData = [
   {field: "keyword", flex : 1},
   {field: "count", flex : 1},
   {field: "density", flex : 1}
 ];
-// const defaultColDef = ref({ flex: 1 });
 
-const keywordDensity = calculateKeywordDensity(props.text);
+const rowData = ref(putRowData(props.text));
 
-const rowData = [];
-for (const { word, count, density } of keywordDensity) {
-  rowData.push({ keyword: word, count: count, density:density });
-}
+const newText = ref(props.text);
 
 </script>
 <template>
-<div class="p-4 m-4" style="height: 500px">
-  <AgGridVue :column-defs="colData" :row-data="rowData" style="height: 100%; width: 75%;" />
+<div class="p-4 m-4 flex flex-row justify-center items-center h-[500px]">
+  <div class="p-2 m-2 flex flex-col items-center gap-8">
+    <MaTextarea
+      v-model="newText"
+      placeholder="Enter your description here..."
+      rows="10"
+      class="w-96">
+    </MaTextarea>
+    <MaButton
+      color="green"
+      icon="coffee-bulk"
+      @click="handleButtonClick"
+    >
+      Keyword Count Density
+    </MaButton>
+  </div>
+  <AgGridVue :column-defs="colData" :row-data="rowData" class="h-3/4 w-3/4" />
 </div>
 </template>
