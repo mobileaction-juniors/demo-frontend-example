a3b9458dae49670024ca9eefe20f79567235657f
Emre <emreeoruc04@gmail.com>
2026-07-23 15:21:30 +0300

    feat(keyword-generator-E2E): add test to remove stop word

--- cypress/e2e/keyword-generator.cy.js
+++ cypress/e2e/keyword-generator.cy.js
@@ -11,9 +11,15 @@ describe("Keyword Generator", () => {
     cy.get("textarea").should("be.visible");
   });
 
+  it("renders the controls", () => {
+    cy.contains("button", "Generate").should("be.visible");
+    cy.contains("button", "Keyword Count Density").should("be.visible");
+    cy.contains("Select n-gram sizes").should("be.visible");
+  });
+
   it("accepts a description", () => {
-    cy.get("textarea").type("hello world hello");
-    cy.get("textarea").should("have.value", "hello world hello");
+    cy.get("textarea").type("hello world");
+    cy.get("textarea").should("have.value", "hello world");
   });
 
   it("toggles the generate button label", () => {
@@ -24,5 +30,14 @@ describe("Keyword Generator", () => {
   it("navigates to the keyword count density page", () => {
     cy.contains("button", "Keyword Count Density").click();
     cy.url().should("include", "/keyword-count-density");
+  });
+
+  it("removes stop words from the description", () => {
+    cy.get("textarea").type("myself and the keyword generator");
+    cy.contains("button", "Generate").click();
+    cy.contains("p", "Cleaned and Filtered Description:")
+      .should("be.visible")
+      .and("contain", "keyword generator")
+      .and("not.contain", "myself");
   });
 });
